ab20d8fff351843f9bc2a2373bcc6cb3e0a1ec34
ToxA <sirrgei.wotrush@gmail.com>
2025-10-26 22:38:31 +0300

    ток начал...

---
+++ src/main/java/net/toxa/bsmod/item/ModItems.java
@@ -0,0 +1,21 @@
+package net.toxa.bsmod.item;
+
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+import net.toxa.bsmod.BS_mod;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, BS_mod.MOD_ID);
+
+    public static final RegistryObject<Item> SHELLYATT = ITEMS.register("shelly_attack",
+            () -> new Item(new Item.Properties()));
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+
+}
